292e5673c43d45e1a6a1e2894b72b7a5c5f86604
Michael Coughlin <michael.coughlin@colorado.edu>
2017-11-26 17:37:38 -0700

    some changes

--- operational_os/experiments/sha256_enclave/sha512_microblaze.c
+++ operational_os/experiments/sha256_enclave/sha512_microblaze.c
@@ -2,5 +2,13 @@
 #include "sha512.h"
 
 void sha512_run(unsigned char *data_in, int *data_len, unsigned char *hash_out){
-  sha512((const unsigned char*)data_in, *data_len, hash_out);
+  int i;
+  unsigned char data[0x3000], hash_result[0x40];
+  for(i=0; i<*data_len; i++){
+    data[i] = data_in[i];
+  }
+  sha512((const unsigned char*)data, *data_len, hash_result);
+  for(i=0; i<0x40; i++){
+    hash_out[i] = hash_result[i];
+  }
 }

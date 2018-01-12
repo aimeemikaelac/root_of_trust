407b7afa7f53735eca92e0cb5cb06a8f5ab0ae78
Michael Coughlin <michael.coughlin@colorado.edu>
2018-01-11 17:50:04 -0700

    moved experiments dir to make scripts work and started build/install scripts

--- sdk/build.sh
+++ sdk/build.sh
@@ -5,11 +5,14 @@ SCRIPTPATH=$(dirname $SCRIPT)
 
 THRIFTPATH=$SCRIPTPATH/thrift
 THRIFTOUT=$SCRIPTPATH/thrift_out
+
+mkdir -p $THRIFTPATH
+wget https://archive.apache.org/dist/thrift/0.10.0/thrift-0.10.0.tar.gz
+tar -xzvf /tmp/thrift-0.10.0.tar.gz -C $THRIFTPATH --strip-components=1
 mkdir -p $THRIFTOUT
 
 cd $THRIFTPATH
 ./bootstrap.sh
-./configure --prefix=$THRIFTOUT --exec-prefix=$THRIFTOUT --with-cpp --without-qt4 --without-qt5 --without-c_glib --without-csharp --without-java --without-erlang --without-nodejs --without-lua --without-python --without-perl --without-php --without-php_extension --without-dart --without-dart --without-ruby --without-haskell --without-go --without-rs --without-haxe --without-dotnetcode --without-d --without-py3 --without-libevent --without-zlib
-# --with-boost=no
+./configure
 make
-make install
+sudo make install
---
+++ sdk/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT=$(realpath $0)
+SCRIPTPATH=$(dirname $SCRIPT)
+
+cd $SCRIPTPATH
+./build.sh
+#thrift --gen cpp enclave_program_to_server.thrift
+#thrift --gen py enclave_program_to_server.thrift
+#mv gen-py gen_py
+sudo apt-get install -y python3-pip
+pip3 install -r requirements.txt

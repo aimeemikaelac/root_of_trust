9cd0f0d227233a6fc53075e85fe483b44266c935
Michael Coughlin <michael.coughlin@colorado.edu>
2018-03-30 16:36:47 -0600

    seperated projects

--- operational_os/hls/contact_discovery_axi_experimental/solution1/script.tcl
+++ operational_os/hls/contact_discovery_axi_experimental/solution1/script.tcl
@@ -3,17 +3,17 @@
 ## Please DO NOT edit it.
 ## Copyright (C) 1986-2017 Xilinx, Inc. All Rights Reserved.
 ############################################################
-open_project contact_discovery_axi
+open_project contact_discovery_axi_experimental
 set_top contact_discovery
-add_files contact_discovery_axi/src/contact_discovery.cpp
-add_files contact_discovery_axi/src/fixedint.h
-add_files contact_discovery_axi/src/sha512.c
-add_files contact_discovery_axi/src/sha512.h
-add_files -tb contact_discovery_axi/src/contact_discovery_tb.cpp
+add_files contact_discovery_axi_experimental/src/contact_discovery.cpp
+add_files contact_discovery_axi_experimental/src/fixedint.h
+add_files contact_discovery_axi_experimental/src/sha512.c
+add_files contact_discovery_axi_experimental/src/sha512.h
+add_files -tb contact_discovery_axi_experimental/src/contact_discovery_tb.cpp
 open_solution "solution1"
 set_part {xczu9eg-ffvb1156-1-i} -tool vivado
 create_clock -period 10 -name default
-#source "./contact_discovery_axi/solution1/directives.tcl"
+#source "./contact_discovery_axi_experimental/solution1/directives.tcl"
 csim_design -compiler gcc
 csynth_design
 cosim_design

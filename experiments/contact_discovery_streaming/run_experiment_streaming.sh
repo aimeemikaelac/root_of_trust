89d6169f3608b29bb05008c79f27e2e7e7c28178
Michael Coughlin <michael.coughlin@colorado.edu>
2018-04-23 10:45:02 -0600

    added experiment and partial results

---
+++ experiments/contact_discovery_streaming/run_experiment_streaming.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT=$(realpath $0)
+SCRIPTPATH=$(dirname $SCRIPT)
+
+ITERATIONS=50
+MAX=1000000
+
+DATA_DIR=$SCRIPTPATH/data/streaming
+
+mkdir -p $DATA_DIR
+OUTPUT_FILE="$DATA_DIR/streaming_exp_$(date +"%Y%m%d%H%M").csv"
+
+make contact_discovery_experiment
+
+echo "ITERATIONS,DB_SIZE,SW_ELAPSED,HW_ELAPSED" | tee $OUTPUT_FILE
+
+for (( size=300; size<$MAX; size*=2 ))
+do
+  sudo $SCRIPTPATH/contact_discovery_experiment $size $ITERATIONS | tee -a $OUTPUT_FILE
+done

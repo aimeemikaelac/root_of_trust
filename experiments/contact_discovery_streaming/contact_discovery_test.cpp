dc239f8424d1310432fc13405200504c62f7e2e2
Michael Coughlin <michael.coughlin@colorado.edu>
2018-05-06 15:26:11 -0600

    got working with memory-mapped db and using buffer-load technique

--- experiments/contact_discovery_streaming/contact_discovery_test.cpp
+++ experiments/contact_discovery_streaming/contact_discovery_test.cpp
@@ -16,15 +16,22 @@
 #define CONTACTS_SIZE 128
 #define DATABASE_SIZE 30000
 
-#define CONTACT_DISCOVERY_BASE 0xB0000000
-#define INPUT_MAPPER_BASE 0xB0010000
-#define RESULTS_MAPPER_BASE 0xB0020000
+#define CONTACT_DISCOVERY_BASE 0xA0000000
+//#define INPUT_MAPPER_BASE 0xB0010000
+#define RESULTS_MAPPER_BASE 0xB0030000
 #define RESULTS_BUFFER 0xB0100000
-#define RESULTS_BUFFER_FPGA 0xA0000000
-#define FPGA_RAM_LOWER 0x00000000
-#define FPGA_RAM_UPPER 0x00000004
-#define RAM_BUFFER 0x400000000
-#define TRANSFER_MAX 8388608
+#define RESULTS_BUFFER_FPGA 0xC0000000
+//#define FPGA_RAM_LOWER 0x00000000
+//#define FPGA_RAM_UPPER 0x00000004
+//#define RAM_BUFFER 0x400000000
+//#define TRANSFER_MAX 8388608
+//#define OFFSET_HASHES 268435456
+#define LOAD_BUFFER 0xB0000000
+#define CDMA_BASE 0xB0010000
+#define TRANSFER_SOURCE 0xB0000000
+#define TRANSFER_DESTINATION_BASE 0x00000000
+#define TRANSFER_SIZE 0x2000
+
 
 
 void contact_discovery(
@@ -52,13 +59,13 @@ void contact_discovery(
   }
   //run match
   // detect if transfer size is too large, e.g. > 2^23
-  if(db_size*64 > TRANSFER_MAX){
-    first_length = TRANSFER_MAX;
-    remaining = db_size - TRANSFER_MAX;
-  } else{
-    first_length = db_size;
-    remaining = 0;
-  }
+//  if(db_size*64 > TRANSFER_MAX){
+//    first_length = TRANSFER_MAX;
+//    remaining = db_size - TRANSFER_MAX;
+//  } else{
+//    first_length = db_size;
+//    remaining = 0;
+//  }
   if(operation == 1){
 //  printf("starting mapper programming\n");
     // program input stream
@@ -84,8 +91,8 @@ void contact_discovery(
     writeValueToAddress(db_size, RESULTS_MAPPER_BASE + 0x58);
   //  printf("finished mapper programming\n");
     //write dma_offset
-    writeValueToAddress(FPGA_RAM_LOWER, CONTACT_DISCOVERY_BASE + 0x5c);
-    writeValueToAddress(FPGA_RAM_UPPER, CONTACT_DISCOVERY_BASE + 0x60);
+    writeValueToAddress(0, CONTACT_DISCOVERY_BASE + 0x5c);
+    writeValueToAddress(0, CONTACT_DISCOVERY_BASE + 0x60);
     //write db size
     writeValueToAddress(db_size, CONTACT_DISCOVERY_BASE + 0x68);
   }
@@ -132,6 +139,7 @@ void contact_discovery(
     printf("Reading results\n");
     for(i=0; i<db_size/4; i++){
       getValueAtAddress(RESULTS_BUFFER + i*4, &results_val);
+//      printf("Result %i: %08x\n", i, results_val);
       bool *results_buffer = (bool*)&results_val;
       for(j=0; j<4; j++){
         matched_out[i*4 + j] = results_buffer[j];
@@ -189,6 +197,29 @@ void hash_numbers(){
   }
 }
 
+void transfer_buffer(long long transfer_offset){
+  unsigned int cdma_status;
+  //verify CDMASR.IDLE = 1
+  cdma_status = 0;
+  while(cdma_status & 0x2 == 0){
+    getValueAtAddress(CDMA_BASE + 0x4, &cdma_status);
+  }
+  //write source address
+  writeValueToAddress(TRANSFER_SOURCE, CDMA_BASE + 0x18);
+  writeValueToAddress(0, CDMA_BASE + 0x1C);
+  //write dest address
+  writeValueToAddress((unsigned int)(transfer_offset), CDMA_BASE + 0x20);
+  writeValueToAddress((unsigned int)(transfer_offset >> 32), CDMA_BASE + 0x24);
+//  printf("Destination address: %08x\n", (unsigned int)(transfer_offset));
+//  printf("Destination address msb: %08x\n", (unsigned int)(transfer_offset >> 32));
+  //write transfer length
+  writeValueToAddress(TRANSFER_SIZE, CDMA_BASE + 0x28);
+  while(cdma_status & 0x2 == 0){
+    getValueAtAddress(CDMA_BASE + 0x4, &cdma_status);
+  }
+}
+
+
 int main(){
   unsigned int seed;
   long long i;
@@ -229,9 +260,19 @@ int main(){
   }
 
   printf("Populating db stream\n");
+  int buffer_index = 0;
+  long long transfer_offset = 0;
   for(i=0; i<DATABASE_SIZE*64/4; i++){
-    writeValueToAddress(((unsigned int*)db_hashes)[i], RAM_BUFFER + i*4);
+    writeValueToAddress(((unsigned int*)db_hashes)[i], LOAD_BUFFER + buffer_index);
+    buffer_index += 4;
+    if(buffer_index >= TRANSFER_SIZE){
+      transfer_buffer(transfer_offset);
+      buffer_index = 0;
+      transfer_offset += TRANSFER_SIZE;
+    }
   }
+  transfer_buffer(transfer_offset);
+  
 
   printf("Checking initial conditions\n");
   //check initial conditions of clearing contacts
